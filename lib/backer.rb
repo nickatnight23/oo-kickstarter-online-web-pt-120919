ca21309de3708fe2ae42cbbc7145b086ebfa7819
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-20 19:41:15 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,14 @@
-backer class
+class Backer
+  attr_reader :name, :backed_projects
 
-def initialize 
-  @backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
 end
